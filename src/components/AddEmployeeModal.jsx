f6404d3e1bded391861e693f6064470fa4c09a5a
Santeri Aaltonen <santeri.aaltonen@pipedrive.com>
2023-10-24 11:55:23 +0300

    Floating labels for modal form

--- src/components/AddEmployeeModal.jsx
+++ src/components/AddEmployeeModal.jsx
@@ -1,4 +1,4 @@
-import { FormGroup, FormLabel } from "react-bootstrap";
+import { FloatingLabel, FormGroup } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
@@ -6,33 +6,32 @@ import Modal from "react-bootstrap/Modal";
 export default function AddEmployeeModal({ show, handleClose, handleShow }) {
   return (
     <Modal show={show} onHide={handleClose}>
-      <Form className="p-3">
+      <Form className="p-4">
         <FormGroup className="mb-3">
-          <FormLabel>Name</FormLabel>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Employee name"
+          <FloatingLabel controlId="floatingName" label="Name" className="mb-3">
+            <Form.Control required type="text" placeholder="" />
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingPosition"
+            label="Position"
             className="mb-3"
-          />
-
-          <FormLabel>Position</FormLabel>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Employee position"
+          >
+            <Form.Control required type="text" placeholder="" />
+          </FloatingLabel>
+          <FloatingLabel label="Tribe" className="mb-3">
+            <Form.Select>
+              <option>Gears</option>
+              <option>InternStelar</option>
+              <option>Other Engineering</option>
+            </Form.Select>
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingDate"
+            label="Start date"
             className="mb-3"
-          />
-
-          <FormLabel>Tribe</FormLabel>
-          <Form.Select className="mb-3">
-            <option>Gears</option>
-            <option>InternStelar</option>
-            <option>Other Engineering</option>
-          </Form.Select>
-
-          <FormLabel>Start date</FormLabel>
-          <Form.Control required type="date" className="mb-3" />
+          >
+            <Form.Control required type="date" />
+          </FloatingLabel>
         </FormGroup>
         <div className="row justify-content-around">
           <Button className="delete-button col-5" onClick={handleClose}>
